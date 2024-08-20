561be5e99188337bbb2c986fc81b2c2f7f2f484c
NikhilMogal <nikhilmogal9@gmail.com>
2024-08-20 16:39:30 +0530

    first commit

---
+++ js/resp.js
@@ -0,0 +1,15 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+navList=document.querySelector('.nav-list')
+rightNav=document.querySelector('.right-nav')
+
+
+burger.addEventListener('click', ()=>{
+
+    rightNav.classList.toggle('v-nav');
+    navList.classList.toggle('v-nav');
+    navbar.classList.toggle('h-nav')
+
+
+
+})
